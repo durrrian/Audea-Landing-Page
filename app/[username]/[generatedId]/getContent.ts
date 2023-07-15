0741c4c67a425d35b6a06ea72a323da1b1a9897b
Furqon Wilogo <fdwilogo@gmail.com>
2023-07-16 00:38:03 +0700

    change to network only

--- app/[username]/[generatedId]/getContent.ts
+++ app/[username]/[generatedId]/getContent.ts
@@ -39,7 +39,7 @@ export const getContent = (
             generatedId,
             username,
           },
-          fetchPolicy: 'no-cache',
+          fetchPolicy: 'network-only',
         });
 
         resolve(getSharedContent);
