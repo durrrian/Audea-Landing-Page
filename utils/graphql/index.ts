e8d81b55ff7eef0a89f013b062b8282fed6e1db6
Furqon Wilogo <fdwilogo@gmail.com>
2023-07-20 19:28:02 +0700

    migrate server to railway

--- utils/graphql/index.ts
+++ utils/graphql/index.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 const client = new ApolloClient({
   uri:
     process.env.NODE_ENV === 'production'
-      ? 'https://audea-server.herokuapp.com/graphql'
+      ? 'https://audea-server-production.up.railway.app/graphql'
       : 'http://localhost:8080/graphql',
   cache: new InMemoryCache(),
 });
