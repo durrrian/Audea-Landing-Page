9824deefc121b06fb97a121b5e377e0aadc2b276
Furqon Wilogo <fdwilogo@gmail.com>
2023-07-29 22:16:04 +0700

    add gtag component

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ViewportProvider } from '@/context/Viewport';
 import { DarkModeProvider } from '@/context/DarkMode';
 import meta from './meta';
 import TTInterfaces from '@/fonts/TTInterfaces';
+import GTag from './gtag';
 
 export const metadata: Metadata = meta;
 
@@ -18,6 +19,7 @@ export default function RootLayout({
       <ViewportProvider>
         <DarkModeProvider>
           <html lang="en" suppressHydrationWarning={true}>
+            <GTag />
             <body className={TTInterfaces.className}>{children}</body>
           </html>
         </DarkModeProvider>
